defc5a9b39990baa1a99623452f9feb8cd14519b
cinthcreative <47701540+cinthcreative@users.noreply.github.com>
2022-05-04 00:14:12 -0700

    Updated App JS, added GitHub repository URL.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello from React</div>;
+  return (
+    <div className="App">
+      <h1>Hello from React</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://cinthiakoutianov.com" target="_blank" rel="noreferrer">
+          Cinthia Koutianov
+        </a>{" "}
+        and it is {""}
+        <a
+          href="https://github.com/cinthcreative/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
