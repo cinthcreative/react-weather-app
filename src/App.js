2b678033031a62bcc74ea293de815fa3a8b7faef
cinthcreative <47701540+cinthcreative@users.noreply.github.com>
2022-05-05 09:48:07 -0700

    Added Bootstrap.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://cinthiakoutianov.com" target="_blank" rel="noreferrer">
-          Cinthia Koutianov
-        </a>{" "}
-        and it is {""}
-        <a
-          href="https://github.com/cinthcreative/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Hello from React</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://cinthiakoutianov.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Cinthia Koutianov
+          </a>{" "}
+          and it is {""}
+          <a
+            href="https://github.com/cinthcreative/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
